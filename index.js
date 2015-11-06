ce6c5047d72ee3a57733cf560bb19cdf6df808d9
Korei Klein <korei.klein@gmail.com>
2015-11-06 11:18:40 -0500

    Write index.js.

--- index.js
+++ index.js
@@ -3,4 +3,27 @@
 
 module.exports = {
   name: 'ember-cli-development-headers'
+
+  serverMiddleware: function(config) {
+    var config = this.project.config(process.env.EMBER_ENV);
+
+    if (!config.developmentHeaders) {
+      console.warn('Please add developmentHeaders to your config.');
+    } else {
+      config.app.use(function(req, res, next) {
+        for (var i = 0; i < config.developmentHeaders.length; ++i) {
+          var header = config.developmentHeaders[i];
+          var key = header['key'];
+          var value = header['value'];
+          if (key && value) {
+            res.setHeader(key, value));
+          } else {
+            console.warn('Improperly formatted header in developmentHeader config. Got:');
+            console.warn(header);
+            console.warn('Expected something of the form { key: "key", value: "value" }');
+          }
+        }
+      });
+    }
+  },
 };
